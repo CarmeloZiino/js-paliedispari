5d9113c03c34d7e060c5367afd75c2b0a8c74148
“CarmeloZiino” <carmeloziino.dev@gmail.com>
2025-01-15 15:02:29 +0100

    Concluso l'esercizio

--- Pari e Dispari/main.js
+++ Pari e Dispari/main.js
@@ -18,14 +18,30 @@ if (sceltaUtente != "pari" && sceltaUtente != "dispari"){ //3. Istruzioni nel ca
     console.log(`Non fare il furbetto! Ricarica la pagina e scegli "Pari" o "Dispari"!`)
     return;
 }
+console.log(`Hai scelto: ${sceltaUtente}`)
 
-let numeroUtente = prompt ("Scegli un numero da 1 a 5:"); //4. Prompt per chiedere all'utente di scegliere un numero fra 1 e 5
+let numeroUtente = parseInt(prompt ("Scegli un numero da 1 a 5:")); //4. Prompt per chiedere all'utente di scegliere un numero fra 1 e 5
 if (numeroUtente > 5 || numeroUtente < 1){ //5. Istruzioni nel caso non seguissero le indicazioni del Prompt
     console.log(`Ancora! Se non ti attieni alle istruzioni, inutile giocare. Ricarica la pagina!`);
     return;
 }
 
+let numeroComputer = parseInt(Math.floor (Math.random ()*5)+1); //6. Genera un numero randomico da 1 a 5 per il Computer
 
+console.log (`Figo! Il tuo numero è: ${numeroUtente}`); //7. Stampa il tuo numero
+console.log (`Mh! Il numero del Computer è: ${numeroComputer}`); //8. Stampa il numero del computer
+
+let somma = numeroUtente + numeroComputer; //9. Faccio una somma del numero scelto dall'utente e del numero randomico per il computer
+console.log (`Totale: ${somma}`);
+
+let risultato; //10. Creo una variabile che calcola il resto della somma dei due numeri
+if (somma % 2 === 0){ //11. Assegno delle istruzioni che permettono di definire se il numero è pari...
+    risultato = "pari";
+} else {risultato = "dispari"} //12. ...o dispari
+
+if (risultato === sceltaUtente){ //13. Creo una Condizione per la quale, se il risultato (pari o dispari) è identico a quello scelto dall'utente, l'utente ha vinto
+    return console.log (`Congratulazioni! Hai vinto!`)
+} else{console.log(`Oh no! Hai perso... riprova. Sarai più... SFORTUNATO`)} //14. Altrimenti ha perso
 
 }
 
