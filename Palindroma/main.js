0ecb72570df55dbfad646a08c52fd0775596fd1b
“CarmeloZiino” <carmeloziino.dev@gmail.com>
2025-01-15 13:06:07 +0100

    Creazione respo

---
+++ Palindroma/main.js
@@ -0,0 +1,10 @@
+// Palidroma Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+
+
+// 1. Creo un Prompt che chiede all'utente di inserire 
+
+
+// 2.
+// 3.
+// 4.
+// 5.
